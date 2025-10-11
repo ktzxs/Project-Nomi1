36fc3559dc87668345c597f71a27706ec4dee928
ktzxs <lucasalvesdacosta070@gmail.com>
2025-10-11 02:39:41 -0300

    criada a páfina de login

---
+++ js/login.js
@@ -0,0 +1,22 @@
+import { FaRegCheckCircle, FaRegTimesCircle } from "react-icons/fa";
+
+function SignUpPage() {
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    //logica para validar a senha em tempo real
+    const validations = {
+        length: password.length >= 8,
+        uppercase: /[A-Z]/.test(password),
+        lowercase:/[a-z]/.test(password),
+        number: /[0-9]/.test(password),
+        special: /[@!&_]/.test(password),
+    }
+
+    const PasswordCriterion = ({isValid, text}) => (
+        <li className={`criterion ${isValid ? 'valid' : ''}`}>
+            {isValid ? <FaRegCheckCircle /> : <FaRegTimesCircle />}
+            <span>{text}</span>
+        </li>
+    )nh
+}
