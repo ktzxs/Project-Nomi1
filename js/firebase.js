ea467d026f3cfc25d4c310e204838dd463d55261
ktzxs <lucasalvesdacosta070@gmail.com>
2025-10-22 15:38:08 -0300

    sistema de login com google não está funcionando

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,33 @@
-import { initializaApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, signWithPopup, signOut } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAeZBjEdRv5mhwk-UuadpdZkzImKdBNdJs",
+  authDomain: "project-nomi-5b550.firebaseapp.com",
+  projectId: "project-nomi-5b550",
+  storageBucket: "project-nomi-5b550.firebasestorage.app",
+  messagingSenderId: "240244825647",
+  appId: "1:240244825647:web:5ab6243b4d82b2a2d230da",
+  measurementId: "G-4N7WC9YTQM",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+async function signInWithGooglePopup() {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    return result.user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function logout() {
+  await signOut(auth);
+}
+
+export { auth, googleProvider, signInWithGooglePopup, logout };
