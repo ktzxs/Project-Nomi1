cce057ee7ec228af7a367dbb1eebe5a78c5bf146
ktzxs <lucasalvesdacosta070@gmail.com>
2025-10-22 14:10:09 -0300

    inicio do backend login

---
+++ js/firebase.js
@@ -0,0 +1,4 @@
+import { initializaApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signWithPopup, signOut } from 'firebase/auth'
+
